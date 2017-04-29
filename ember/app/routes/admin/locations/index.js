abd53a66d197eb29d30bd5c4c1704eace1a82d86
Dino Prašo <desmond1303@gmail.com>
2017-04-29 17:27:30 +0200

    Fix JSHint Warnings

--- ember/app/routes/admin/locations/index.js
+++ ember/app/routes/admin/locations/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
-  model(params) {
+  model() {
     return Ember.RSVP.hash({
       locations: this.get('ajax').request('/getAllCities'),
     });
--- ember/app/routes/admin/users/index.js
+++ ember/app/routes/admin/users/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
-  model(params) {
+  model() {
     return Ember.RSVP.hash({
       users: this.get('ajax').request('/admin/getAllUsers', {
         xhrFields: {
